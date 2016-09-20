c974f4e79e8a80b9d007eccbf2ecd74fe988957e
tterrag1098 <tterrag1098@gmail.com>
2016-09-19 20:17:06 -0400

    Better organization, split header/nav into separate html

---
+++ web/home.php
@@ -0,0 +1,27 @@
+
+<?php
+    $title = "SU Math/CS Club Home";
+    include("includes/header.html");
+    include("includes/sidenav.html");
+?>
+
+<body>
+
+<div id="main">
+
+<div id="content">
+
+<img src="images/mathCSbanner.png" alt="mathCSbannerIMG" class="homeImage"/><br>
+<br>
+
+Welcome to the Salisbury University Mathematics and Computer Science Club Website, featuring the Dead Poet's Society! The club is open to everyone - majors, minors, and anyone else in the SU
+community with an interest in mathematics, computer science, and much more.
+We host a wide variety of events including: talks, field trips, coding competitions, math challenge competitions, pumpkin carving, Association for Computing Machinery (<a href="https://www.acm.org/">ACM</a>) meetings, and more!
+<p>Club meetings currently take place in Henson 101 at 6pm every Thursday. </p>
+
+</div>
+</div>
+
+</body>
+
+</html>
