9259ed4144e48fb76b86b1c6c3ff86e80ae4ffcb
tterrag1098 <tterrag1098@gmail.com>
2016-09-27 15:20:43 -0400

    Begin login backend

---
+++ web/classes/Login.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * Class login
+ * handles the user's login and logout process
+ */
+class Login
+{
+    require_once(__DIR__."/../includes/database.php");
+
+    /**
+     * @var array Collection of error messages
+     */
+    public $errors = array();
+    /**
+     * @var array Collection of success / neutral messages
+     */
+    public $messages = array();
+
+    /**
+     * the function "__construct()" automatically starts whenever an object of this class is created,
+     * you know, when you do "$login = new Login();"
+     */
+    public function __construct()
+    {
+        // create/read session, absolutely necessary
+        session_start();
+
+        // check the possible login actions:
+        // if user tried to log out (happen when user clicks logout button)
+        if (isset($_GET["logout"])) {
+            $this->doLogout();
+        }
+        // login via post data (if user just submitted a login form)
+        elseif (isset($_POST["login"])) {
+            $this->dologinWithPostData();
+        }
+    }
+
+    /**
+     * log in with post data
+     */
+    private function dologinWithPostData()
+    {
+        // check login form contents
+        if (empty($_POST['user_name'])) {
+            $this->errors[] = "Username field was empty.";
+        } elseif (empty($_POST['user_password'])) {
+            $this->errors[] = "Password field was empty.";
+        } elseif (!empty($_POST['user_name']) && !empty($_POST['user_password'])) {
+
+/* Not needed?
+            // change character set to utf8 and check it
+            if (!$this->db_connection->set_charset("utf8")) {
+                $this->errors[] = $this->db_connection->error;
+            }
+*/
+
+                // database query, getting all the info of the selected user (allows login via email address in the
+                // username field)
+                Array cols = ("email", "password");
+                $result = $db->where("email", $user_name)->getOne("users", null, $cols);
+
+                // if this user exists
+                if ($result) {
+
+                    // using PHP 5.5's password_verify() function to check if the provided password fits
+                    // the hash of that user's password
+                    if (password_verify($_POST['user_password'], $result['password'])) {
+
+                        // write user data into PHP SESSION (a file on your server)
+                        $_SESSION['user_email'] = $result['email'];
+                        $_SESSION['user_login_status'] = 1;
+
+                    } else {
+                        $this->errors[] = "Wrong password. Try again.";
+                    }
+                } else {
+                    $this->errors[] = "This user does not exist.";
+                }
+        }
+    }
+
+    /**
+     * perform the logout
+     */
+    public function doLogout()
+    {
+        // delete the session of the user
+        $_SESSION = array();
+        session_destroy();
+        // return a little feeedback message
+        $this->messages[] = "You have been logged out.";
+
+    }
+
+    /**
+     * simply return the current state of the user's login
+     * @return boolean user's login status
+     */
+    public function isUserLoggedIn()
+    {
+        if (isset($_SESSION['user_login_status']) AND $_SESSION['user_login_status'] == 1) {
+            return true;
+        }
+        // default return
+        return false;
+    }
+}
--- web/login.php
+++ web/login.php
@@ -9,20 +9,24 @@
 
 <div class = "login-background">
 <div class ="login-transbox">
-<form action="process-login.php" enctype= multipart/form-data> 
-    <p>
-    	Email  
-    	<br>
-    	<input style="width:200px;" name="email" type="text" class="input-box" required>
-    	<br>
-    	Password 
-    	<br> 
-    	<input style="width:200px;" name = "password" type = "text" class="input-box" required>
-    	<br>
-    	<a href=""> <!-- link to change password -->forgot password? </a>
-        <input type="submit" value="login" class="loginSubmit" name ="member-login">  
-        </p>                            
+
+<form method="post" action="/" name="loginform">
+
+      <label for="login_input_email">Email</label>
+    	<input style="width:200px;" id="login_input_email" class="input-box" name="user_email" type="text" required>
+
+      <p>
+
+      <label for="login_input_password">Password</label>
+      <input style="width:200px;" id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required />
+
+      <p>
+
+    	<a href="#"> <!-- link to change password -->forgot password? </a>
+
+      <input type="submit" value="login" class="loginSubmit" name ="Log in">
 </form>
+
 </div> <!-- end login-transbox -->
 </div> <!-- end login-background -->
 
