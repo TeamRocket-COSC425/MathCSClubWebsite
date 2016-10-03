1eb47d8076773c24b810e96fe2de54db2806fbee
michaelrh37 <michaelrh37@gmail.com>
2016-10-03 12:24:56 -0400

    Started working on officers circle

--- web/officers.php
+++ web/officers.php
@@ -18,7 +18,36 @@
 <h1>
 Officers
 </h1>
+<div class="center">
+<p>This is some text above the circles for testing purposes</p>
 
+<!-- content to be placed inside <body>…</body> -->
+<div class='circle-container'>
+	<a class='groupPic'><img src="images/officers/fall16septemberOfficers.jpg"></a>
+	<a class='president'><img src="images/officers/chelsey.jpg"></a>
+	<a class='treasurer'><img src="images/officers/billy.jpg"></a>
+	<a class='webMaster'><img src="images/officers/dustin.jpg"></a>
+	<a class='secretary'><img src="images/officers/rachel.jpg"></a>
+	<a class='representative'><img src="images/officers/sam.png"></a>
+	<a class='vicePresident'><img src="images/officers/billy.jpg"></a>
+    <a class='DPS'><img src="images/officers/billy.jpg"></a>
+</div>
+
+
+
+
+
+<!--<div class="officerCircle">
+<div id="big-circle" class="circle big"></div>
+    <div class="circle one"></div>
+    <div class="circle two"></div>
+    <div class="circle three"></div>
+    <div class="circle four"></div>
+    <div class="circle five"></div>
+    <div class="circle six"></div>
+    <div class="circle seven"></div>
+</div>-->
+</div>
 </div>
 </div>
 
