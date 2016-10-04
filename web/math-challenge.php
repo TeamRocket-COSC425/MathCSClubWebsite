225a8c4dc0aa3023007553c8f99a948b4358fa18
JdaMenace <jdeminnis@gmail.com>
2016-10-04 15:21:17 -0400

    added the math challenege page and imags with css

--- web/math-challenge.php
+++ web/math-challenge.php
@@ -1,12 +1,13 @@
 <?php
-    $title = "SU Math/CS Club Math Challenge";
+    $title = "SU Math/CS Club GullCode";
     include("includes/header.html");
     include("includes/sidenav.html");
-    include("includes/Login.html");
+    include("includes/topnav.php");
+
 ?>
 
 <head>
-	<title>Math CS Club - Math Challenge</title>
+	<title>Math CS Club - GullCode</title>
 </head>
 
 <body>
@@ -15,17 +16,57 @@
 
 <div id="content">
 
-<h1>
-Math Challenge
-</h1>
+<!--Top header line -->
+<header>
+<code> MATH CHALLENGE</code>
+</header>
 
-<?php
-include("includes/Register.html");
-?>
+<br>
+
+<!-- About container  -->
+ <div class="floated_left">
+ 	<center><b> About </b></center>
+ 	The Math Challenge is new to Salisbury. Everyone who signs up for the math challenge will
+	be given problems to solve in a given time limit. 
+	awards will be given out for the individual portion; First Place between Freshman and Sophomores
+	&amp; First Place between Juniors and Seniors. In the group portion, teams of three will be given a set
+	of problems to be solved in a set amount of time. There will be a award for First Place, Second
+	Place, Third Place, and Most Creative Problem Solving Team.
+ </div>
+<!-- End About container -->
+<br>
+<br>
+
+<!--Images container -->
+ <div class ="floated_middle">
+   	<img src = "images/activities/meetingsp2015.jpg" class="gullcodepic">
+	<img src ="images/math-challenege/mc-triangles.jpg"  			 class ="gullcodepic">
+</div>
+<!--End Images container-->
+
+
+<!--Rules container -->
+ <div class="floated_right">
+    <center><b><h3>Rules</h3></b></center>
+	There will be a individual and group portion.<br>
+	<b><u>Individual</u></b><br>
+		<ul>
+			<li>Each individual will recieve a certain number of problems. </li>
+			<li>Everyone who signs up will recieve a number of problems.</li>
+			<li>There is a time limit</li>
+		</ul>
+ </div>
+<!--End Rules container-->
+
+
+
+
+
 
 </div>
+<!--End content container-->
 </div>
-
+<!--End main container-->
 </body>
 
 </html>
