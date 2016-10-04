fa2dfbba6d90adee12c4dfbe56d4cd8611ff2130
michaelrh37 <michaelrh37@gmail.com>
2016-10-04 15:30:08 -0400

    Finished the sizing and positioning of officers

--- web/officers.php
+++ web/officers.php
@@ -18,23 +18,21 @@
 <h1>
 Officers
 </h1>
-<div class="center">
 <p>This is some text above the circles for testing purposes</p>
 
-
 <div class='circle-container'>
-	<a class='groupPic'><img src="images/officers/fall16septemberOfficers.jpg"></a>
-	<a class='president'><img src="images/officers/chelsey.jpg"></a>
+	<div class='groupPic'><img src="images/officers/fall16septemberOfficers.jpg"></div>
+	<a class='president'><img src="images/officers/chelsey2.jpg"></a>
 	<a class='treasurer'><img src="images/officers/billy.jpg"></a>
 	<a class='webMaster'><img src="images/officers/dustin.jpg"></a>
 	<a class='secretary'><img src="images/officers/rachel.jpg"></a>
 	<a class='representative'><img src="images/officers/sam.png"></a>
-	<a class='vicePresident'><img src="images/officers/billy.jpg"></a>
-    <a class='DPS'><img src="images/officers/billy.jpg"></a>
+	<a class='vicePresident'><img src="images/officers/questionMark.png"></a>
+    <a class='DPS'><img src="images/officers/questionMark.png"></a>
 </div>
 </div>
 </div>
-</div>
+
 
 </body>
 
