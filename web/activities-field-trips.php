f283651f23cba8a9900c66aadb80e152466cebf8
Casey <csmith20@gulls.salisbury.edu>
2016-10-05 13:00:46 -0400

    photo modifications, activities page updates

--- web/activities-field-trips.php
+++ web/activities-field-trips.php
@@ -7,83 +7,95 @@
 
 <head>
 	<title>Math CS Club - Activities &amp; Field Trips</title>
-
-<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <script type='text/javascript' src='js/infinite-rotator.js'></script>
-    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="css/activities.css"/>
+	<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 </head>
+
+
+
 
 <body>
 
 <div id="main">
 
 <div id="content">
-    <!-- START: Rotating Images -->
-    <div id="activities-rotating-item-wrapper">
-      <img src="images/meeting/fall16september.jpg" alt="meetingpicture" class="activities-rotating-item" />
-      <img src="images/activities/meetingsp2015.jpg" alt="multicultural day" class="activities-rotating-item" />
-      <img src="images/activities/image3.jpg" alt="Wiffle ball" class="activities-rotating-item" />
-      <img src="images/activities/IMG_1895.jpg" alt="chicken dinner" class="activities-rotating-item" />
-      <img src="images/activities/oriolessp2016.png" alt="activities fair" class="activities-rotating-item" />
-      <img src="images/activities/slaughterhousefa2015.jpg" alt="tower of hanoi" class="activities-rotating-item" />
-    </div>
-    <!-- END: Rotating images images -->
+
+    <img src="images/meeting/fall16september.jpg" alt="meetingpicture" class="mySlides w3-animate-fading"/>
+    <img src="images/activities/meetingsp2015.jpg" alt="multicultural day" class="mySlides w3-animate-fading"/>
+	<img src="images/activities/image3.jpg" alt="Wiffle ball" class="mySlides w3-animate-fading"/>
+  	<img src="images/activities/oriolessp2016.png" alt="activities fair" class="mySlides w3-animate-fading"/> 
+  	<img src="images/activities/IMG_1895.jpg" alt="chicken dinner" class="mySlides w3-animate-fading"/>
+
 <h1 class="center">
 Activites &amp; Field Trips
 </h1>
 
-<h2 class="center">
-Fall 2016 
+<h2>
+Fall 2016
 </h2>
 
-<h3 style="text-align: left;">
-Orioles Game
-</h3>
+<!--EVENTS NEED TO BE DYNAMICALLY ALLOCATED IN THE DB SO PRES CAN ADD MORE AND STUFF-->
 
-<p>
-<strong>Date:</strong> Friday September 23rd 2016<br>
-<strong>Price:</strong> $20 per person <br>
-<strong>Availability:</strong> 30 tickets for sale <br>
-<strong>Transportation:</strong> Van will be provided along with car poolers <br>
-<strong>Details:</strong> Game starts at 7:05pm and we will leave the GUC parking lot at 4pm. THe Orioles will be playing the Arizona Diamondbacks. It is a fan appreciation night and everyone will recieve a Hyun Soo Kim T-shirt and fireworks will take place after the game. <br>
-<strong>What you need to do:</strong> Contact Sam at <u>smaillie1@gulls.salisbury.edu</u> to RSVP and pay. <br>
+<button class="accordion">Orioles Game</button>
+<div class="panel">
+	<b>Date:</b> Friday September 23<sup>rd</sup>, 2016<br>
+	<b>Price:</b> $20 per person <br>
+	<b>Availability:</b> 30 tickets for sale <br>
+	<b>Transportation:</b> Van will be provided along with car poolers <br>
+	<b>Details:</b> Game starts at 7:05pm and we will leave the GUC parking lot at 4pm. The Orioles will be playing the Arizona Diamondbacks. It is a fan appreciation night and everyone will recieve a Hyun Soo Kim T-shirt and fireworks will take place after the game. <br>
+	<b>What you need to do:</b> Contact Sam at <u>smaillie1@gulls.salisbury.edu</u> to RSVP and pay. <br>
 <br>
-<img src="images/activities/camdenyards.jpg" alt="Camden Yards" width="100%">
+<img src="images/activities/camdenYards.jpg" alt="Camden Yards"></div>
+
+<button class="accordion">Slaughter House Farm</button>
+<div class="panel">
+  <b>Date:</b> Friday October 14<sup>th</sup> 2016<br>
+	<b>Price:</b> $15 per person <br>
+	<b>Availability:</b> 21 seats available <br>
+	<b>Transportation:</b> Van will be provided along with car poolers <br>
+	<b>Details:</b> Meet at 6:15pm in Henson 101. Event takes place in Laurel, DE (about 25 minutes away) and ends by 11pm.<br>
+	<b>What you need to do:</b> Contact Chelsey at <u>cclement1@gulls.salisbury.edu</u> to RSVP and pay. <br>
+</div>
+
+<button class="accordion">Section 3</button>
+<div class="panel">
+  <p>Lorem ipsum...</p>
+</div>
+
+
 
 </p>
 </div>
 </div>
 
+
 <script>
-$(window).load(function() {	//start after HTML, images have loaded
-	var InfiniteRotator = 
-	{
-		init: function()
-		{
-			var initialFadeIn = 1000; //initial fade-in time (in milliseconds)
-			var itemInterval = 5000; //interval between items (in milliseconds)
-			var fadeTime = 2500; //cross-fade time (in milliseconds)
-			var numberOfItems = $('.activities-rotating-item').length; //count number of items
-			var currentItem = 0; //set current item
-			$('.activities-rotating-item').eq(currentItem).fadeIn(initialFadeIn); //show first item
+var myIndex = 0;
+carousel();
 
-			//loop through the items
-			var infiniteLoop = setInterval(function(){
-				$('.activities-rotating-item').eq(currentItem).fadeOut(fadeTime);
-				if(currentItem == numberOfItems -1){
-					currentItem = 0;
-				}else{
-					currentItem++;
-				}
-				$('.activities-rotating-item').eq(currentItem).fadeIn(fadeTime);
-			}, itemInterval);	
-		}	
-	};
-	InfiniteRotator.init();
-});
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 9000);
+}
+
+//script for accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
+}
 </script>
-
 </body>
 
 </html>
