ac1d75fabc02c09d75fda38fc4b872b263272413
michaelrh37 <michaelrh37@gmail.com>
2016-10-18 17:31:18 -0400

    Working on officers bios

--- web/officers.php
+++ web/officers.php
@@ -26,9 +26,15 @@ Meet Your Officers
 	<?php
       	$positions = $db->get('officers');
       	foreach($positions as $pos) {
-        	echo '<a  class="'. $pos["position"] .'"> <img src="' . $pos["image"] . '" ></a>';                    
+        	echo '<a class="'. $pos["position"] .'"> <img src="' . $pos["image"] . '" ></a>';                    
     	  }
     ?>
+
+
+<div class='officer-bio'>
+    <H2>Chelsey Clement<br>President</H2>
+    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a</p>
+</div>
 </div>
 </div>
 </div>
