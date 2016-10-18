9ee1fd61477f0a2d443ee6a7ef09f419e16ae712
michaelrh37 <michaelrh37@gmail.com>
2016-10-18 14:06:49 -0400

    Working on officers stuff

--- web/officers.php
+++ web/officers.php
@@ -31,6 +31,14 @@ Meet Your Officers
     ?>
 </div>
 
+ <div class="bubble">
+        <h2>President</h2>
+        <!--<p>I’m also the Chair/Founder of SU ACM Chapter. Currently, I am a Data Analyst for Perdue Farms Inc. This will be my first year as an SI for Math 213 (Statistical Thinking) and Tutor for Project Enchant. I teach advanced tap, modern, jazz, and ballet at Xtreme Dance Studio. In my free time, I ride my motorcycle and take pictures for B&ampB Snapshots.</p>-->
+        <p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack. Quick wafting zephyrs vex bold Jim. Quick zephyrs blow, vexing daft Jim. for quick jigs vex!</p>
+</div>
+
+
+
 <!--<form method="post" name="officerPic">
     <p class="message">Enter URL for picture</p>
     <input id="input_officer_pic" name="officer_pic" type="text" placeholder="URL" required>
