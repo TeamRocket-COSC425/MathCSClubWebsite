e2c1f94de6fbbc3fbf9cf6bb9c36944d227ddd4f
Casey <csmith20@gulls.salisbury.edu>
2016-11-02 12:51:24 -0400

    bad merge :(

--- web/includes/topnav.php
+++ web/includes/topnav.php
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-<link rel="stylesheet" href="css/topnav.css"/>
-<div class="col-6">
-<?php
-  require_once("classes/Login.php");
-  require_once("classes/Utils.php");
-
-  $login = new Login();
-
-  echo '<div id="topnav">';
-  if ($login->isUserLoggedIn()) {
-    $admin = Utils::currentUserAdmin();
-    if ($admin) {
-      include("views/Edit.html");
-    }
-    include("views/Logout.html");
-    include("views/Dashboard.html");
-    echo '<div id="sessiondata"> <i class="fa fa-user" aria-hidden="true"></i>
- You are logged in as <b><a href="profile">'. $_SESSION['user_email'] . '</a>' . ($admin ? ' (Admin)' : '') . '</b></div>';
-  } else {
-    include("views/SignUp.html");
-    include("views/Login.html");
-  }
-  echo '</div>';
-?>
-</div>
-=======
 <link rel="stylesheet" href="css/topnav.css"/>
 
 <?php
@@ -50,4 +23,3 @@ if ($login->isUserLoggedIn()) {
 }
   echo '</div>';
 ?>
->>>>>>> master
