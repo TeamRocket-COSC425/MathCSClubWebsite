21d29566e8549e10e12f7e3e04750c7652ca30b3
Whoffman23 <Whhoffman194@gmail.com>
2016-11-03 16:18:25 -0400

    redirection page

--- web/classes/registration-gc-mc.php
+++ web/classes/registration-gc-mc.php
@@ -1,145 +1,108 @@
 <?php
-require_once __DIR__."/../includes/database.php";
+    require_once __DIR__."/../includes/database.php";
     require_once("classes/Utils.php");
 
 
 
 class math_challenge{
-	 public $errors = array();
-
-    public function __construct()
-    {
+    public $errors = array();
+    public function __construct(){
         if (isset($_POST["mc-register"])) {
             $this->registerNewMathChallenge();
         }
     }
 
-public function teamcheck($id)
-{
-    global $db;
-    $teamid = $db->where('team_name',$id)->getOne('math_challenge_teams');
-    print_r($teamid);
-    if($teamid)
-    {
-       $members = $db->where('team_id', $teamid['team_id'])->get('math_challenge_users_on_teams');
-        if(sizeof($members) < 3)
-        {
-            return true;
+    public function teamcheck($id){
+        global $db;
+        $teamid = $db->where('team_name',$id)->getOne('math_challenge_teams');
+        print_r($teamid);
+        if($teamid){
+            $members = $db->where('team_id', $teamid['team_id'])->get('math_challenge_users_on_teams');
+            if(sizeof($members) < 3){
+                return true;
+            }
+            else{
+                return false;
+            }
         }
-        else
-        {
-            return false;
-        }
+
+        return true;
     }
 
-    return true;
-}
-
- private function registerNewMathChallenge()
-    {
+    private function registerNewMathChallenge(){
     	$user = Utils::getCurrentUser();
         global $db;
 
          $or = $_POST['registert-as'];       
      
-        if( $or != 0 && strlen($_POST['team-name']) > 4 && strlen($_POST['team-name']) < 32 && $this->teamcheck($_POST['team-name']))
-        {
+        if( $or != 0 && strlen($_POST['team-name']) > 4 && strlen($_POST['team-name']) < 32 && $this->teamcheck($_POST['team-name'])){
            
-            $data = array(
-
-                    'team_name' => $_POST['team-name']);
+            $data = array('team_name' => $_POST['team-name']);
             $id = $db -> insert("math_challenge_teams",$data);
 
             $team = $db->where("team_name", $_POST['team-name'])->getOne("math_challenge_teams");
-             $data = array(
-                    'id'=> $user['id'],
-                    'team_id' => $team['team_id']
-                    );
+            $data = array('id'=> $user['id'],'team_id' => $team['team_id']);
            $id = $db->insert('math_challenge_users_on_teams',$data);
         }
-
-            elseif ($or != 0 && strlen($_POST['team-name']) < 4) {
-             echo "<br><br><center><div  style='color:red; width:20%; background-color:white; border-color:black;border-style: ridge;border-width:auto;  padding: 6px 12px;'><b>Error:</b><br>Math Challenge Team Name is too short</center>";}
-         
-         elseif ($or != 0 && strlen($_POST['team-name']) > 32) {
-               echo "<br><br><center><div  style='color:red; width:10%; background-color:white; border-color:black;border-style: ridge;border-width:auto;  padding: 6px 12px;'>Error:Math Challenge Team Name is too long</center>";}
-
-        elseif($or != 0 && !$this->teamcheck($_POST['team-name']))
-        {
+        elseif ($or != 0 && strlen($_POST['team-name']) < 4) {
+             echo "<br><br><center><div  style='color:red; width:20%; background-color:white; border-color:black;border-style: ridge;border-width:auto;  padding: 6px 12px;'><b>Error:</b><br>Math Challenge Team Name is too short</center>";
+        }
+        elseif ($or != 0 && strlen($_POST['team-name']) > 32) {
+            echo "<br><br><center><div  style='color:red; width:10%; background-color:white; border-color:black;border-style: ridge;border-width:auto;  padding: 6px 12px;'>Error:Math Challenge Team Name is too long</center>";
+        }
+        elseif($or != 0 && !$this->teamcheck($_POST['team-name'])){
             echo "<br><br><center><div  style='color:red; width:10%; background-color:white; border-color:black;border-style: ridge;border-width:auto;  padding: 6px 12px;'>Error:The team, ". $_POST['team-name'] .", is full, please join/make another team.</center>";
         }
-              
         else{
-        	$data = array('id' => $user['id'],
-                    'team_id' => 0 );
-                    
+        	$data = array('id' => $user['id'],'team_id' => 0 );
             $id = $db->insert('math_challenge_users_on_teams', $data);
-}
+        }
             
-             $data = array(
-
-                    't_size' => $_POST['t-size']);
-             $db->where("id", $user['id']);
-            $id = $db ->update('users',$data);
-      
+        $data = array('t_size' => $_POST['t-size']);
+        $db->where("id", $user['id']);
+        $id = $db ->update('users',$data);
     }
 }
-class gullcode{
-     public $errors = array();
 
-    public function __construct()
-    {
+class gullcode{
+    public $errors = array();
+
+    public function __construct(){
         if (isset($_POST["gc-register"])) {
             $this->registerNewGullCode();
         }
     }
 
-public function teamcheck($id)
-{
-    global $db;
-    $teamid = $db->where('team_name',$id)->getOne('gullcode_teams');
-    print_r($teamid);
-    if($teamid)
-    {
-       $members = $db->where('team_id', $teamid['team_id'])->get('gullcode_users_on_teams');
-        if(sizeof($members) < 3)
-        {
-            return true;
+    public function teamcheck($id){
+        global $db;
+        $teamid = $db->where('team_name',$id)->getOne('gullcode_teams');
+        print_r($teamid);
+        if($teamid){
+            $members = $db->where('team_id', $teamid['team_id'])->get('gullcode_users_on_teams');
+            if(sizeof($members) < 3){
+                return true;
+            }
+            else{
+                return false;
+            }
         }
-        else
-        {
-            return false;
-        }
+        return true;
     }
 
-    return true;
-}
-
-
-
-
- private function registerNewGullCode()
-    {
+    private function registerNewGullCode(){
         $user = Utils::getCurrentUser();
         global $db;
 
-         $or = $_POST['registert-as'];       
+        $or = $_POST['registert-as'];       
      
-        if( $or != 0 && strlen($_POST['team-name']) > 4 && strlen($_POST['team-name']) < 32 && $this->teamcheck($_POST['team-name']))
-        {
+        if( $or != 0 && strlen($_POST['team-name']) > 4 && strlen($_POST['team-name']) < 32 && $this->teamcheck($_POST['team-name'])){
            
-            $data = array(
-
-                    'team_name' => $_POST['team-name']);
+            $data = array('team_name' => $_POST['team-name']);
             $id = $db -> insert("gullcode_teams",$data);
 
             $team = $db->where("team_name", $_POST['team-name'])->getOne("gullcode_teams");
-             $data = array(
-                    'id'=> $user['id'],
-                    'team_id' => $team['team_id']
-                    );
-           $id = $db->insert('gullcode_users_on_teams',$data);
-
+            $data = array('id'=> $user['id'],'team_id' => $team['team_id']);
+            $id = $db->insert('gullcode_users_on_teams',$data);
         }
         elseif ($or != 0 && strlen($_POST['team-name']) < 4) {
              echo "<br><br><center><div  style='color:red; width:10%; background-color:white; border-color:black;border-style: ridge;border-width:auto;  padding: 6px 12px;'>Error:<br>GullCode Team Name is too short</center>";
@@ -151,24 +114,15 @@ public function teamcheck($id)
         {
             echo "<br><br><center><div  style='color:red; width:10%; background-color:white; border-color:black;border-style: ridge;border-width:auto;  padding: 6px 12px;'>Error:The team, ". $_POST['team-name'] .", is full, please join/make another team.</center>";
         }
-                
         else{
-            $data = array('id' => $user['id'],
-                    'team_id' => 0 );
+            $data = array('id' => $user['id'],'team_id' => 0 );
                     
             $id = $db->insert('gullcode_users_on_teams', $data);
         }
 
-   
-             $data = array(
-
-                    't_size' => $_POST['t-size']);
-             $db->where("id", $user['id']);
-            $id = $db ->update('users',$data);
-
-      
+        $data = array('t_size' => $_POST['t-size']);
+        $db->where("id", $user['id']);
+        $id = $db ->update('users',$data);
     }
 }
- 
-
 ?>
--- web/gc-mc-registration.php
+++ web/gc-mc-registration.php
@@ -1,27 +1,47 @@
 <?php
+  require_once("classes/Login.php");
+  require_once("classes/Utils.php");
+  $login = new Login();
+  if ($login->isUserLoggedIn()) {
+  	$user = Utils::getCurrentUser();
+    $gcmembers = $db->get("gullcode_users_on_teams");
+    $mcmembers = $db->get("math_challenge_users_on_teams");
+    foreach ($gcmembers as $gcmember) {
+      if($gcmember['id'] == $user['id']) {
+        header("Location: gc-mc-notification");
+        die();
+      }
+    }
+    foreach ($mcmembers as $mcmember) {
+      if($mcmember['id'] == $user['id']) {
+        header("Location: gc-mc-notification");
+        die();
+      }
+    }
+  } 
+  else {
     $title = "SU Math/CS Club Registration";
     include("includes/header.html");
     include("includes/sidenav.html");
     include("includes/topnav.php");
-require_once('classes/registration-gc-mc.php');
+    require_once('classes/registration-gc-mc.php');
     $gc = new gullcode();
     $mc = new math_challenge();
+  }
 ?>
 
 <head>
 	<title>Math CS Club - GullCode/Math Challenge Registration</title>
-	  <link rel="stylesheet" href="css/forms.css"/>
-	  <link rel="stylesheet" href="css/gc-mc-registration.css">
-
+	<link rel="stylesheet" href="css/forms.css"/>
+	<link rel="stylesheet" href="css/gc-mc-registration.css">
 </head>
 
 <body class="gc-mc-background">
-<div class="container">
-<br><br><br><br><br>
+  <div class="container">
+    <br><br><br>
   
 <!-- Begin Tabs navigation -->
 <ul class = "tab">
-
   <a href="javascript:void(0)"  onclick="openTab(event,'About Me')" id="defaultOpen"></a>
   <li><a href="javascript:void(0)" class="tablinks" onclick="openTab(event, 'Math-Challenge')">
   <h1>Math Challenge</h1></a></li>
@@ -30,141 +50,121 @@ require_once('classes/registration-gc-mc.php');
 </ul>
 
 <div id="About Me" class="tabcontent">
-         <center><strong> Select Competition Link Above </strong> </center>
-      </div>
+  <center><strong> Select Competition Link Above </strong> </center>
+</div>
+
 <!--Math Challenge Tab Content-->
 <?php 
-if($login->isUserLoggedIn()) {
+  if($login->isUserLoggedIn()) {
     $user = Utils::getCurrentUser();
 
-        $members = $db->get("math_challenge_users_on_teams");
-        $check = 0 ;
-        foreach ($members as $member) {
-            if($member['id'] == $user['id']) {
-                $check = 1;
-            }
-
-        }
-        if($check == 1) {
-            echo("<div id='Math-Challenge' class='tabcontent'>
-              <p class='center' style='color:red'><img src='images/message-icons/error.gif' width='50'style='float: left; margin: -.25em .5em 2em 2em '><span><b>You have already registered for Math Challenge.<br>Check your profile for more info</b></p>
-              </div>");
-        }
-        else{
-          ?>
-<div id="Math-Challenge" class="tabcontent">
-<center><strong><u>Sign up Form</u></strong></center>
- 
-	<form method="post">
-		 	
-		<select id="registert-as" name="registert-as" required>
-	    	<option> Choose how to Sign up </option>
-		    <option value="0">Free Agent</option>
-		    <option value="1">Team</option>
-    	
-		</select>
-	
-		<div id="team">
-        	<input type="text" placeholder="Team Name" name="team-name" />
-    	</div>
-		
-		<br>
-			T-Shirt Size:
-		<select name="t-size">
-			<optgroup label="Class">
-	  			<option value="0">Small</option>
-	  			<option value="1">Medium</option>
-	  			<option value="2">Large</option>
-	  			<option  value="3">x-large</option>
-	  			<option value="4">2x-large</option>
-  			</optgroup>
-		</select>
-       
-
-         <input name="mc-register" type="submit" value="Register for Math Challenge" class="insert"/>
-
-
-		</form>
-
-
-
-</div><?php
-        }
+    $members = $db->get("math_challenge_users_on_teams");
+    $check = 0 ;
+    foreach ($members as $member) {
+      if($member['id'] == $user['id']) {
+        $check = 1;
       }
-          ?>
- 
-	
+    }
 
-<!-- GullCode Tab Content-->
-<?php 
-if($login->isUserLoggedIn()) {
-    $user = Utils::getCurrentUser();
-
-        $members = $db->get("gullcode_users_on_teams");
-        $check = 0 ;
-        foreach ($members as $member) {
-            if($member['id'] == $user['id']) {
-                $check = 1;
-            }
-
-        }
-        if($check == 1) {
-             echo("<div id='GullCode' class='tabcontent'>
-              <p class='center' style='color:red'><img src='images/message-icons/error.gif' width='50'style='float: left; margin: -.25em .5em 2em 2em '><span><b>You have already registered for GullCode.<br>Check your profile for more info</b></p>
-              </div>");
-        }
-        else{
-             ?>
-<div id="GullCode" class="tabcontent">
-<center><strong><u>Sign up Form</u></strong></center>
- 
-	<form method="post">
-		 	
-		<select id="registert-as" name="registert-as" required>
-	    	<option> Choose how to Sign up </option>
-		    <option value="0">Free Agent</option>
-		    <option value="1">Team</option>
-    	
-		</select>
-	
-		<div id="team">
-        	<input type="text" placeholder="Team Name" name="team-name" />
-    	</div>
-		
-		<br>
-			T-Shirt Size:
-		<select name="t-size">
-			<optgroup label="Class">
-	  			<option value="0">Small</option>
-	  			<option value="1">Medium</option>
-	  			<option value="2">Large</option>
-	  			<option  value="3">x-large</option>
-	  			<option value="4">2x-large</option>
-  			</optgroup>
-		</select>
-       
-
-         <input name="gc-register" type="submit" value="Register for GullCode" class="insert"/>
-
-
-		</form>
-
-
-
-</div><?php
-        }
-  }
+    if($check == 1) {
+      echo("<div id='Math-Challenge' class='tabcontent'><p class='center' style='color:red'><img src='images/message-icons/error.gif' width='50'style='float: left; margin: -.25em .5em 2em 2em '><span><b>You have already registered for Math Challenge.<br>Check your profile for more info</b></p></div>");
+    }
+    else{
 ?>
 
+<div id="Math-Challenge" class="tabcontent">
+  <center><strong><u>Sign up Form</u></strong></center>
+ 	<form method="post">	
+	 <select id="registert-as" name="registert-as" required>
+	   <option> Choose how to Sign up </option>
+	   <option value="0">Free Agent</option>
+	   <option value="1">Team</option>
+    </select>
+	
+		<div id="team">
+        	<input type="text" placeholder="Team Name" name="team-name" />
+    </div>
+		
+		<br>
+		
+    T-Shirt Size:
+		<select name="t-size">
+			<optgroup label="Class">
+	  			<option value="0">Small</option>
+	  			<option value="1">Medium</option>
+	  			<option value="2">Large</option>
+	  			<option  value="3">x-large</option>
+	  			<option value="4">2x-large</option>
+  		</optgroup>
+		</select>
+       
+    <input name="mc-register" type="submit" value="Register for Math Challenge" class="insert"/>
+	</form>
+</div>	
+<?php
+	}
+	}
+?>
+<!-- GullCode Tab Content-->
+<?php 
+  if($login->isUserLoggedIn()) {
+    $user = Utils::getCurrentUser();
 
+    $members = $db->get("gullcode_users_on_teams");
+    $check = 0;
+    foreach ($members as $member) {
+      if($member['id'] == $user['id']) {
+        $check = 1;
+      }
+    }
+    
+    if($check == 1) {
+      echo("<div id='GullCode' class='tabcontent'><p class='center' style='color:red'><img src='images/message-icons/error.gif' width='50'style='float: left; margin: -.25em .5em 2em 2em '><span><b>You have already registered for GullCode.<br>Check your profile for more info</b></p></div>");
+    }
+    else{
+?>
+
+<div id="GullCode" class="tabcontent">
+  <center><strong><u>Sign up Form</u></strong></center>
+ 	<form method="post">
+		<select id="registert-as" name="registert-as" required>
+	    	<option> Choose how to Sign up </option>
+		    <option value="0">Free Agent</option>
+		    <option value="1">Team</option>
+		</select>
+	
+		<div id="team">
+        	<input type="text" placeholder="Team Name" name="team-name" />
+   	</div>
+		
+		<br>
+
+		T-Shirt Size:
+		<select name="t-size">
+			<optgroup label="Class">
+	  			<option value="0">Small</option>
+	  			<option value="1">Medium</option>
+	  			<option value="2">Large</option>
+	  			<option  value="3">x-large</option>
+	  			<option value="4">2x-large</option>
+ 			</optgroup>
+		</select>
+       
+    <input name="gc-register" type="submit" value="Register for GullCode" class="insert"/>
+	</form>
+</div>
+<?php
+	}
+}
+?>
+</div>
 <script>
-document.getElementById("defaultOpen").click();
-
-function openTab(evt, cityName) {
+  document.getElementById("defaultOpen").click();
+  function openTab(evt, cityName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+      tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
@@ -172,32 +172,26 @@ function openTab(evt, cityName) {
     }
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
-}
+  }
 </script>
 
 <script>
-
-$(document).ready(function () {
+  $(document).ready(function () {
     toggleFields(); // call this first so we start out with the correct visibility depending on the selected form values
     // this will call our toggleFields function every time the selection value of our other field changes
     $("#registert-as").change(function () {
-        toggleFields();
+     toggleFields();
     });
-
-});
-// this toggles the visibility of other server
-function toggleFields() {
-    if ($("#registert-as").val() == "1")
-        $("#team").show();
-    else
+  });
+  // this toggles the visibility of other server
+  function toggleFields() {
+    if ($("#registert-as").val() == "1") {
+      $("#team").show();
+    }
+    else {
         $("#team").hide();
-}
+    }
+  }
 </script>
-
-</div>
-
-
-
-                                        
-</body>
-</html>
+           
+</body>                  
