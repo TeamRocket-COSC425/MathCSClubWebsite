38dae711ec25bc449581fa9e0691601c7de9de44
Casey <csmith20@gulls.salisbury.edu>
2016-11-04 15:43:34 -0400

    top nav drop down

--- web/includes/topnav.php
+++ web/includes/topnav.php
@@ -7,8 +7,44 @@
   $login = new Login();
 
   echo '<div id="topnav">';
+  echo '<div id="topnav-mobile">';
 if ($login->isUserLoggedIn()) {
     $admin = Utils::currentUserAdmin();
+?>
+
+    <i class="fa fa-caret-down" id="mobile-dropdown" aria-hidden="true"></i>
+<script>
+  var swapped = false;
+
+  function reverseButtons() {
+    $("#mobile-toggle > a").each(function(){
+      $(this).prependTo(this.parentNode);
+    });
+  }
+
+  function checkSize() {
+    if (!swapped && $("#mobile-toggle").css("clear") == "both") {
+      reverseButtons();  
+      swapped = true;
+    } else if (swapped && $("#mobile-toggle").css("clear") == "none") {
+      reverseButtons();
+      swapped = false;
+    }
+  }
+  
+  $(document).ready(function(){
+    $("#mobile-dropdown").click(function(){
+      $("#mobile-toggle").slideToggle();
+    });
+    checkSize();
+    
+    $(window).resize(checkSize);
+  });
+</script>
+
+
+<?php
+echo '<div id="mobile-toggle">';
     if ($admin) {
         include "views/Edit.html";
     }
@@ -21,5 +57,7 @@ if ($login->isUserLoggedIn()) {
     include "views/SignUp.html";
     include "views/Login.html";
 }
+  echo '</div>';
+  echo '</div>';
   echo '</div>';
 ?>
