0db86e9e575151707e1e4184364a1cbf7d2dd03f
Casey <csmith20@gulls.salisbury.edu>
2016-11-08 16:19:26 -0500

    change logged in button to my profile. home is also mobile friendly

--- web/home.php
+++ web/home.php
@@ -28,17 +28,16 @@
 <div id="content">
 <?php (new EditableContent("home"))->getContent(); ?>
 
-
+ <div class="fb-page" data-href="https://www.facebook.com/sumathcoscclub" data-tabs="timeline" 
+ data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" 
+ data-show-facepile="true" float="left"><blockquote cite="https://www.facebook.com/sumathcoscclub" class="fb-xfbml-parse-ignore">
+ <a href="https://www.facebook.com/sumathcoscclub">SU Math &amp; COSC Club</a></blockquote></div>
 <div id="video-container">
-<div class="fb-page" data-href="https://www.facebook.com/sumathcoscclub" data-tabs="timeline" 
-data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" 
-data-show-facepile="true" float="left"><blockquote cite="https://www.facebook.com/sumathcoscclub" class="fb-xfbml-parse-ignore">
-<a href="https://www.facebook.com/sumathcoscclub">SU Math &amp; COSC Club</a></blockquote></div>
-this is an example of one of the events we host, geopong!
-<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Frandy.cone%2Fvideos%2F10154655043466350%2F&show_text=0&width=560" 
-width="650" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
-</div>
-
+  <br>
+<h2>Here's an example of one of the events we host, geopong!<h2>
+ <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Frandy.cone%2Fvideos%2F10154655043466350%2F&show_text=0&width=560" 
+ width="650" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+ </div>
  </div>
 
 </div>
