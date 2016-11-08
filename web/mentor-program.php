b1ae6f951730057a84c4ca3dff04cf06bb6e1fc8
mjohnson98 <mjohnson23@gulls.salisbury.edu>
2016-11-08 16:58:00 -0500

    made mentor picture link to mentor profile page

--- web/mentor-program.php
+++ web/mentor-program.php
@@ -49,7 +49,7 @@ Mentor Program
 				echo '<td><table id="mentors" align="center">';
 				echo "<th colspan='1'>" . $mentor['name'] . "</th>";
 				echo '<tr><td> <img src="' . $mentor["image"] . '" class=\'mentorpic\' alt="' .
-				$mentor["name"] . '" onclick="document.forms[0].elements[\'Mentor Info\'].value = \'' . $mentor["name"] . '\n\n' . $mentor["major"] . '\n\n' . Utils::year($mentor["year"]) . '\n\n' . $mentor["bio"] . '\'"> </td></tr></table></td>';
+				$mentor["name"] . '" onclick="window.location=\'profile.php?user='. $mentor["id"] . '\'"> </td></tr></table></td>';
 				$count= $count - 1;
 			}
 			else
@@ -57,7 +57,7 @@ Mentor Program
 				echo '<td><table id="mentors" align="center">';
 				echo "<th colspan='1'>" . $mentor['name'] . "</th>";
 				echo '<tr><td> <img src="' . $mentor["image"] . '" class=\'mentorpic\' alt="' .
-				$mentor["name"] . '" onclick="document.forms[0].elements[\'Mentor Info\'].value = \'' . $mentor["name"] . '\n\n' . $mentor["major"] . '\n\n' . Utils::year($mentor["year"]) . '\n\n' . $mentor["bio"] . '\'"> </td></tr></table></td>';
+				$mentor["name"] . '" onclick="window.location=\'profile.php?user='. $mentor["id"] . '\'"> </td></tr></table></td>';
 				if ($ogcount > "3"){
 					echo "</tr><tr>";
 				}
@@ -68,27 +68,27 @@ Mentor Program
 ?>
 <br>
 
-<form class="center">
-<textarea class="mentorinfo" name="Mentor Info" placeholder="Select a Mentor" readonly>
-</textarea>
-</form>
-+<?php
+
+<?php
 	if($login->isUserLoggedIn())
 	{
 		$user = Utils::getCurrentUser();
-		if ($user['year']<"2")
+		if ($user['year']<2)
 		{
 			include ("views/Mentee.html");
 		}
-		else if (is_null($user['bio']) && $user['year']>"1" && $user['mentor'] = 0)
+		else if (empty($user['image']) && empty($user['bio']) && $user['year']>1 && $user['mentor'] == 0)
+		{
+			echo("<p class ='center' style='color:red;'>You must have a bio and image to become a mentor</p>");
+		}else if (empty($user['bio']) && $user['year']>1 && $user['mentor'] == 0)
 		{
 			echo("<p class ='center' style='color:red;'>You must have a bio to become a mentor</p>");
 		}
-		else if (is_null($user['image']) && $user['year']>"1" && $user['mentor'] = 0)
+		else if (empty($user['image']) && $user['year']>1 && $user['mentor'] == 0)
 		{
 			echo("<p class ='center' style='color:red;'>You must have an image to become a mentor</p>");
 		}
-		else if ($user['year']>"1" && $user['mentor'] = 0)
+		else if (!empty($user['bio']) && !empty($user['image']) && $user['year']>1 && $user['mentor'] == 0)
 		{
 			include ("views/Mentor.html");
 		}
