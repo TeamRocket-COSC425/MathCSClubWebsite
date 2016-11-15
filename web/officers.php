c43bf414b78a64e43b00630f9d6963436e1c6d17
michaelrh37 <michaelrh37@gmail.com>
2016-11-15 17:02:33 -0500

    Fixed the css on the officers page issue #106 (#109)
    So whoever did the original css on the officers page had no idea what they
    were doing so I remade a lot of the css on the page so it looks better.

--- web/officers.php
+++ web/officers.php
@@ -33,13 +33,26 @@ Meet Your Officers
 
 <div class='circle-container'>
 	<div class='groupPic'><img src="images/officers/fall16septemberOfficers.jpg"></div>
-	<?php
+    <?php
       	$positions = $db->get('officers');
       	foreach($positions as $pos) {
-            echo '<a id="'. $pos["position"] .'"> <img src="' . $pos["image"] . '" onclick="toggleByID(\''. $pos["position"] .'-bio\');"></a>';                    
+            echo '<div class="officer" id="'. $pos["position"] .'"> <img src="' . $pos["image"] . '" onclick="toggleByID(\''. $pos["position"] .'-bio\');"></div>';                    
     	  }
     ?>
+</div>
 
+<div id='info'>
+<?php
+        $advisors = $db->get('club_advisors');
+        foreach($advisors as $advisor) {
+            echo '<div id=\'' . $advisor["position"] . '\' class=\'advisor\'>';
+            echo '<div class=\'advisorPic\'><img src=\'' . $advisor["image"] . '\'></div>';
+            echo '<div class=\'advisorInfo\'>';
+            echo '<H3>' . $advisor["name"] . '</H3>';
+            echo '<H4>' . $advisor["title"] . '</H4>';
+            echo '</div></div>';
+        }
+?>
 <div id='officer-bio'>
 
     <div id='default'>
@@ -58,18 +71,6 @@ Meet Your Officers
 
     ?>
 </div>
-
-    <?php
-        $advisors = $db->get('club_advisors');
-        foreach($advisors as $advisor) {
-            echo '<div id=\'' . $advisor["position"] . '\' class=\'advisor\'>';
-            echo '<div class=\'advisorPic\'><img src=\'' . $advisor["image"] . '\'></div>';
-            echo '<div class=\'advisorInfo\'>';
-            echo '<H3>' . $advisor["name"] . '</H3>';
-            echo '<H4>' . $advisor["title"] . '</H4>';
-            echo '</div></div>';
-        }
-    ?>
 </div>
 </div>
 </div>
