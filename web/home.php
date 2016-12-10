bfbff6c4dacc7b0896802b5fb3430bd201ebe4e2
tterrag1098 <tterrag1098@gmail.com>
2016-12-10 17:20:11 -0500

    Improve main page layout/loading

--- web/home.php
+++ web/home.php
@@ -30,17 +30,21 @@
 <?php (new EditableImage("home_banner"))->getContent(); ?>
 <?php (new EditableText("home"))->getContent(); ?>
 
- <div class="fb-page" data-href="https://www.facebook.com/sumathcoscclub" data-tabs="timeline"
- data-small-header="false" data-adapt-container-width="true" data-hide-cover="false"
- data-show-facepile="true" float="left"><blockquote cite="https://www.facebook.com/sumathcoscclub" class="fb-xfbml-parse-ignore">
- <a href="https://www.facebook.com/sumathcoscclub">SU Math &amp; COSC Club</a></blockquote></div>
-<div id="video-container">
-  <br>
-<h2>Here's an example of one of the events we host, GeoPong!<h2>
- <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Frandy.cone%2Fvideos%2F10154655043466350%2F&show_text=0&width=560"
- width="650" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
- </div>
- </div>
+<div id="home-widgets">
+    <div class="fb-page" data-href="https://www.facebook.com/sumathcoscclub" data-tabs="timeline"
+        data-small-header="false" data-adapt-container-width="true" data-hide-cover="false"
+        data-show-facepile="true" float="left">
+        <blockquote style="display:none;" cite="https://www.facebook.com/sumathcoscclub" class="fb-xfbml-parse-ignore">
+            <a href="https://www.facebook.com/sumathcoscclub">SU Math &amp; COSC Club</a>
+        </blockquote>
+    </div>
+    <div id="video-container">
+        <h2>Here's an example of one of the events we host, GeoPong!</h2>
+        <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Frandy.cone%2Fvideos%2F10154655043466350%2F&show_text=0&width=560"
+            width="600" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true">
+        </iframe>
+    </div>
+</div>
 
 </div>
 </div>
