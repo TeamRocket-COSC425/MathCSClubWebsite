c4433a26ab2bef8f2af4b5e7d0b508b55dfecdee
michaelrh37 <michaelrh37@gmail.com>
2017-02-07 13:17:43 -0500

    Fixed contact us form

--- web/contact-us.php
+++ web/contact-us.php
@@ -1,7 +1,9 @@
 <?php
 $title = "SU Math/CS Club Contact Us";
 require_once("vendor/autoload.php");
-    //get form data
+
+$errorMsg = "";       //holds error messages
+  //get form data
 if(isset($_POST['contact_name'])){
   $contact_name = $_POST['contact_name'];
   $contact_email = $_POST['contact_email'];
@@ -12,7 +14,8 @@ if(isset($_POST['contact_name'])){
     $result=false;
 
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-      echo '<script>alert("The e-mail address you provided is not valid."); return false;</script>';
+      //echo '<script>alert("The e-mail address you provided is not valid."); return false;</script>';
+      $errorMsg = "The e-mail address you provided is not valid";
       return $result;
     }
 
@@ -23,19 +26,10 @@ if(isset($_POST['contact_name'])){
   }
 
     //verify email is an email
-  if(validateEmail($_POST['contact_email']) == true){
-    //verify fields have entries
-    if (empty($contact_name)){
-      echo '<script type="text/javascript">alert("Please include your name.");</script>';
-    } elseif (empty($contact_email)){
-      echo '<script type="text/javascript">alert("Please include your email.");</script>';
-    } else if(empty($contact_subject)){
-      echo "Please include a subject for your message.";
-    } else if(empty($contact_message)){
-      echo "Please include your message.";
-    }
-
-    else{
+  if(validateEmail($_POST['contact_email']) == false){
+    $errorMsg = "The e-mail address you provided is not valid";
+  }
+  else if(validateEmail($_POST['contact_email']) == true){
       $mail = new PHPMailer;
 
       $mail->isSMTP();
@@ -55,16 +49,15 @@ if(isset($_POST['contact_name'])){
 
       $mail->setFrom($contact_email);
       $mail->addAddress('sumathcsclub@gmail.com');
-    $mail->addCC($contact_email); //to cc the user 
-    $mail->Subject = $contact_subject;
-    $mail->Body = $contact_message;
+      $mail->addCC($contact_email); //to cc the user 
+      $mail->Subject = $contact_subject;
+      $mail->Body = $contact_message;
 
     if ($mail->send()) {
       // Assure this code only runs once takes you to thankyou page
       header('Location: contactThanks');
       exit();
-    }
-  } 
+    } 
 }
 }
 include("includes/header.html");
@@ -89,6 +82,11 @@ include("includes/topnav.php");
         <p>*All fields are required*</p>
         <br>
 
+        <!--Display error messages-->
+        <div class="loginErrors" style="color:red;">
+          <?php if( isset($errorMsg) && $errorMsg != '' ) { echo $errorMsg; } ?>
+        </div>
+        <br>
 
         <form method="POST">
 
